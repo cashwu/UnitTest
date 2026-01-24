2d9d08027c781698229cba8bb991d84bba99f365
cash <cashwugeek@icloud.com>
2026-01-24 18:51:28 +0800

    Remove NetFramework directory

--- NetFramework/Lab02/Model/ILog.cs
+++
@@ -1,7 +0,0 @@
-namespace Lab02.Model
-{
-    public interface ILog
-    {
-        void Send(string message);
-    }
-}
--- NetFramework/Lab02/Model/Member.cs
+++
@@ -1,7 +0,0 @@
-namespace Lab02.Model
-{
-    public class Member
-    {
-        public string Password { get; set; }
-    }
-}
