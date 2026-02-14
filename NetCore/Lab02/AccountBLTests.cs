afd3961a3256765a764436ba6a1d244c129fe4dd
cash <cashwugeek@icloud.com>
2026-02-14 10:36:12 +0800

    Lab02 login is valid test - using NSubstitute stub

--- NetCore/Lab02/AccountBLTests.cs
+++ NetCore/Lab02/AccountBLTests.cs
@@ -1,5 +1,6 @@
 using FluentAssertions;
 using Lab02.Model;
+using NSubstitute;
 using Xunit;
 
 namespace Lab02;
@@ -10,39 +11,22 @@ public class AccountBLTests
     public void Login_is_valid()
     {
         // test  
-        var accountDao = new FakeAccountDao();
+        var accountDao = Substitute.For<IAccountDao>();
 
-        accountDao.Member = new Member
-        {
-            Password = "sha-1234"
-        };
+        accountDao.GetMemberForLogin("cash")
+                  .Returns(new Member
+                  {
+                      Password = "sha-1234"
+                  });
 
-        var cryptography = new FakeCryptography();
-        cryptography.ShaPassword = "sha-1234";
+        var cryptography = Substitute.For<ICryptography>();
+
+        cryptography.CashSha("cash123456")
+                    .Returns("sha-1234");
 
         var accountBL = new AccountBL(accountDao, cryptography);
 
         var isValid = accountBL.Login("cash", "cash123456");
         isValid.Should().BeTrue();
-    }
-}
-
-public class FakeAccountDao : AccountDao
-{
-    public Member Member { get; set; }
-
-    public override Member GetMemberForLogin(string account)
-    {
-        return Member;
-    }
-}
-
-public class FakeCryptography : Cryptography
-{
-    public string ShaPassword { get; set; }
-
-    public override string CashSha(string password)
-    {
-        return ShaPassword;
     }
 }
