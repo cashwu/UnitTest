fcd260b2606413dc8e967e0f1b5c07cd77fd9b83
cash <cashwugeek@icloud.com>
2026-02-14 14:02:53 +0800

    Lab01 refactor test

--- js/test/dateUtility.test.js
+++ js/test/dateUtility.test.js
@@ -1,17 +1,28 @@
-import {describe, it, expect, vi} from 'vitest'
+import {describe, it, expect, vi, beforeEach} from 'vitest'
 import {DateUtility} from "../src/Lab01/dateUtility";
 
 describe("date utility", () => {
+    let dateUtility = new DateUtility();
+    let fake_getToday = vi.fn();
+
+    beforeEach(() => {
+        dateUtility = new DateUtility();
+        fake_getToday = vi.fn();
+        dateUtility.getToday = fake_getToday;
+    })
 
     it("today is payday", () => {
-
-        let dateUtility = new DateUtility();
-        let fake_getToday = vi.fn();
-        dateUtility.getToday = fake_getToday;
-        fake_getToday.mockReturnValueOnce(new Date(2026, 2, 5));
-
-        expect(dateUtility.isPayday()).toBe(true);
+        givenToday(5);
+        todayShouldBePayday();
     })
+
+    function todayShouldBePayday() {
+        expect(dateUtility.isPayday()).toBe(true);
+    }
+
+    function givenToday(date) {
+        fake_getToday.mockReturnValueOnce(new Date(2026, 2, date));
+    }
 
 });
 
