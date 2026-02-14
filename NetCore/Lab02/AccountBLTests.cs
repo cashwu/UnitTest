3e0dbe6b6d2502972b83d2d3bbcbf3a053cb79a2
cash <cashwugeek@icloud.com>
2026-02-14 10:39:48 +0800

    Lab02 login is valid test - refactor test

--- NetCore/Lab02/AccountBLTests.cs
+++ NetCore/Lab02/AccountBLTests.cs
@@ -7,26 +7,46 @@ namespace Lab02;
 
 public class AccountBLTests
 {
+    private readonly IAccountDao _accountDao;
+    private readonly ICryptography _cryptography;
+    private readonly AccountBL _accountBL;
+
+    public AccountBLTests()
+    {
+        _accountDao = Substitute.For<IAccountDao>();
+        _cryptography = Substitute.For<ICryptography>();
+        _accountBL = new AccountBL(_accountDao, _cryptography);
+    }
+
     [Fact]
     public void Login_is_valid()
     {
         // test  
-        var accountDao = Substitute.For<IAccountDao>();
+        GivenMemberForLogin("cash", new Member
+        {
+            Password = "sha-1234"
+        });
 
-        accountDao.GetMemberForLogin("cash")
-                  .Returns(new Member
-                  {
-                      Password = "sha-1234"
-                  });
+        GivenShaPassword("cash123456", "sha-1234");
 
-        var cryptography = Substitute.For<ICryptography>();
+        LoginShouldBeValid("cash", "cash123456");
+    }
 
-        cryptography.CashSha("cash123456")
-                    .Returns("sha-1234");
-
-        var accountBL = new AccountBL(accountDao, cryptography);
-
-        var isValid = accountBL.Login("cash", "cash123456");
+    private void LoginShouldBeValid(string account, string password)
+    {
+        var isValid = _accountBL.Login(account, password);
         isValid.Should().BeTrue();
+    }
+
+    private void GivenShaPassword(string password, string shaPassword)
+    {
+        _cryptography.CashSha(password)
+                     .Returns(shaPassword);
+    }
+
+    private void GivenMemberForLogin(string account, Member member)
+    {
+        _accountDao.GetMemberForLogin(account)
+                   .Returns(member);
     }
 }
